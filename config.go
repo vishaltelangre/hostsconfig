c41bd31715f7e5b8db9f0c6329b7087836e1f3e4
Vishal Telangre <the@vishaltelangre.com>
2015-11-28 18:32:15 +0530

    Add advanced features, viz. add host(s), delete by ip, host(s), save changes

--- config.go
+++ config.go
@@ -1,28 +1,137 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"log"
+	"net"
+	"os"
+	"regexp"
 	"strings"
 	"text/template"
+)
+
+const (
+	humanizedTemplate = "./templates/humanized"
+	standardTemplate  = "./templates/standard"
+
+	creditsLine = `
+
+# ===========================================================
+# This file is generated using 'hostsconfig' utility.
+# We recommend to use the same tool to modify this file.
+#
+# Download it from here:
+# https://github.com/vishaltelangre/hostsconfig
+# ===========================================================
+`
 )
 
 type Config struct {
 	HostEntries []HostEntry
 }
 
-func (c *Config) AddEntry(e HostEntry) []HostEntry {
+func (c *Config) Parse() {
+	file, err := os.Open(hostsFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		c.AddEntryFromLine(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (c *Config) SaveChanges() {
+	file, err := os.OpenFile(hostsFilePath, os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprint(c.StandardPreview(), creditsLine))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\n\n Hosts file successfully updated!\n\n")
+}
+
+func (c *Config) AddEntry(e HostEntry) {
 	for _, entry := range c.HostEntries {
 		// Add host names into host entry if ip address already exists
 		if entry.IP.String() == e.IP.String() {
 			entry.addHostnames(e.Hostnames)
-			return c.HostEntries
+			return
 		}
 	}
 
 	c.HostEntries = append(c.HostEntries, e)
-	return c.HostEntries
+	return
+}
+
+func (c *Config) AddEntryFromLine(line string) {
+	if entry := parseLine(line); !entry.isEmpty() {
+		c.AddEntry(entry)
+		return
+	}
+}
+
+func (c *Config) DeleteEntryFromLine(line string) {
+	ip := net.ParseIP(line)
+	if ip != nil {
+		c.DeleteEntry(parseLine(line))
+	}
+
+	line = strings.ToLower(strings.TrimSpace(stripComment(line)))
+	c.deleteHostnames(line)
+}
+
+func (c *Config) DeleteEntryAtIndex(i int) {
+	ip := c.HostEntries[i].IP
+	ensureIPIsNotLoopback(ip)
+
+	copy(c.HostEntries[i:], c.HostEntries[i+1:])
+	c.HostEntries[len(c.HostEntries)-1] = HostEntry{}
+	c.HostEntries = c.HostEntries[:len(c.HostEntries)-1]
+}
+
+func (c *Config) DeleteEntryWithIP(ip net.IP) {
+	ensureIPIsNotLoopback(ip)
+
+	for i, entry := range c.HostEntries {
+		if entry.IP.String() == ip.String() {
+			c.DeleteEntryAtIndex(i)
+			return
+		}
+	}
+}
+
+func (c *Config) DeleteEntry(e HostEntry) {
+	for i, entry := range c.HostEntries {
+		if entry.IP.String() == e.IP.String() {
+			entry.deleteHostnames(e.Hostnames)
+
+			// Delete entry if it has no hosts
+			if len(entry.Hostnames) <= 0 {
+				c.DeleteEntryAtIndex(i)
+			}
+
+			// Delete an entry with asked IP address
+			if len(e.Hostnames) <= 0 {
+				c.DeleteEntryAtIndex(i)
+			}
+
+			return
+		}
+	}
 }
 
 func (c *Config) IPv4Entries() []HostEntry {
@@ -47,15 +156,15 @@ func (c *Config) IPv6Entries() []HostEntry {
 	return ipv6Entries
 }
 
-func (c *Config) StandardPreview() {
-	preview(c, standardTemplate, "standard")
+func (c *Config) StandardPreview() string {
+	return preview(c, standardTemplate, "standard")
 }
 
-func (c *Config) PrettyPreview() {
-	preview(c, humanizedTemplate, "humanized")
+func (c *Config) BeautifiedPreview() string {
+	return preview(c, humanizedTemplate, "humanized")
 }
 
-func preview(config *Config, tmpl string, tmplBase string) {
+func preview(config *Config, tmpl string, tmplBase string) string {
 	buf := new(bytes.Buffer)
 	t := template.New(tmplBase)
 	t, err := t.ParseFiles(tmpl)
@@ -72,5 +181,45 @@ func preview(config *Config, tmpl string, tmplBase string) {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("\n%s\n\n", strings.TrimSpace(buf.String()))
+	return fmt.Sprintf("%s", strings.TrimSpace(buf.String()))
+}
+
+func (c *Config) deleteHostnames(line string) {
+	hostnames := make([]Hostname, 0)
+	for _, name := range strings.Fields(line) {
+		hostnames = append(hostnames, Hostname{Name: name})
+	}
+
+	hostEntriesCopy := make([]HostEntry, len(c.HostEntries))
+	copy(hostEntriesCopy, c.HostEntries)
+
+	for _, entry := range hostEntriesCopy {
+		entry.deleteHostnames(hostnames)
+
+		// Delete an entry if it has no hosts
+		if len(entry.Hostnames) <= 0 {
+			c.DeleteEntryWithIP(entry.IP)
+		}
+	}
+}
+
+func ensureIPIsNotLoopback(ip net.IP) {
+	if ip.IsLoopback() {
+		log.Fatalf("'%s' is a loopback IP, can't delete it\n", ip.String())
+	}
+}
+
+func stripComment(line string) string {
+	re := regexp.MustCompile("#.*")
+	line = re.ReplaceAllString(line, "")
+	return line
+}
+
+func parseLine(line string) HostEntry {
+	entry := HostEntry{}
+	entry.rawLine = strings.ToLower(strings.TrimSpace(stripComment(line)))
+	if !entry.isEmpty() {
+		entry.parse()
+	}
+	return entry
 }
--- hostentry.go
+++ hostentry.go
@@ -12,34 +12,60 @@ type HostEntry struct {
 	rawLine   string
 }
 
-func (e *HostEntry) addHostnames(names []Hostname) []Hostname {
+func (e *HostEntry) addHostnames(names []Hostname) {
 	for _, hostname := range names {
 		hostname.IsCanonical = false
 		e.AddHostname(hostname)
 	}
-
-	return e.Hostnames
 }
 
-func (e *HostEntry) AddHostname(h Hostname) []Hostname {
+func (e *HostEntry) AddHostname(h Hostname) {
 	for _, hostname := range e.Hostnames {
 		// Ignore if its a duplicate host name
 		if hostname.Name == h.Name {
-			return e.Hostnames
+			return
 		}
 	}
 
 	e.Hostnames = append(e.Hostnames, h)
-	return e.Hostnames
+}
+
+func (e *HostEntry) deleteHostnames(names []Hostname) {
+	for _, hostname := range names {
+		e.DeleteHostname(hostname)
+	}
+}
+
+func (e *HostEntry) DeleteHostname(h Hostname) bool {
+	if h.Name == "localhost" {
+		log.Fatal("'localhost' is a loopback host can't delete it")
+	}
+
+	for i := range e.Hostnames {
+		if e.Hostnames[i].Name == h.Name {
+			copy(e.Hostnames[i:], e.Hostnames[i+1:])
+			e.Hostnames[len(e.Hostnames)-1] = Hostname{}
+			e.Hostnames = e.Hostnames[:len(e.Hostnames)-1]
+
+			// Make sure that first entry should be canonical
+			if i == 0 && len(e.Hostnames) > 0 {
+				hostname := &e.Hostnames[0]
+				hostname.IsCanonical = true
+			}
+
+			return true
+		}
+	}
+	return false
 }
 
 func (e *HostEntry) parse() {
-	tokens := strings.Fields(e.rawLine)
-	ip := net.ParseIP(tokens[0])
-	hostnames := tokens[1:]
+	fields := strings.Fields(e.rawLine)
+	ip := net.ParseIP(fields[0])
+	hostnames := fields[1:]
 
 	if ip == nil {
-		log.Fatal("%s is invalid IP address", tokens[0])
+		log.Fatalf("'%s' is an invalid IP address", fields[0])
 	}
 
 	e.IP = ip
